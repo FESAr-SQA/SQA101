33f27d0357025f1f2ed74b3c436d9b92fca1009e
Eduardo Sánchez <eduxsan@outlook.com>
2024-01-12 08:29:28 -0600

    Add ublocker to automation framework

--- Utils/driver_factory.py
+++ Utils/driver_factory.py
@@ -3,7 +3,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from Helpers.webdriver_listener import WebDriverListener
 from Extensions.webdriver_extensions import WebDriverExtended
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
 class DriverFactory:
     @staticmethod
@@ -11,10 +12,11 @@ class DriverFactory:
         if config["browser"] == "chrome":
             options = webdriver.ChromeOptions()
             options.add_argument("start-maximized")
+            options.add_extension("Extensions//chrome_extensions//ublock.crx")
             if config["headless_mode"] is True:
                 options.add_argument("--headless")
 
-            service = Service(ChromeDriverManager().install())
+            service = ChromeService(ChromeDriverManager().install())
             driver = WebDriverExtended(
                 webdriver.Chrome(service=service, options=options),
                 WebDriverListener(), config
@@ -23,10 +25,12 @@ class DriverFactory:
             return driver
         elif config["browser"] == "firefox":
             options = webdriver.FirefoxOptions()
+            #TODO: Investigar como añadir extensiones al webdriver de Firefox 
             if config["headless_mode"] is True:
                 options.headless = True
+            service = FirefoxService(GeckoDriverManager().install())
             driver = WebDriverExtended(
-                webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options),
+                webdriver.Firefox(service=service, options=options),
                 WebDriverListener(), config
             )
             return driver
